c46108ab5bbfb1c96fe7f4234a8d025875492cbe
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2021-03-22 02:55:52 +0100

    instantiate event defined in drl

--- test/home_automation/core_test.clj
+++ test/home_automation/core_test.clj
@@ -4,6 +4,18 @@
 
 (def kie-container (init-kie-container))
 
+(defn create-event
+  [ks source action]
+  (let [fact-type (.getFactType (.getKieBase kie-container) "home_automation.rules" "Event")
+        instance (.newInstance fact-type)]
+    (.setFromMap fact-type instance { "source" source "action" action })
+    instance))
+
 (deftest init-knowledge-session-test
   (testing "Initialisation of the KIE container"
-    (is (some? (init-knowledge-session kie-container) true))))
+    (is (some? (init-knowledge-session kie-container)))))
+
+(deftest create-event-test
+  (testing "Create an Event instance declared in Drools"
+    (is (some? (create-event (init-knowledge-session kie-container) "x.y.z" "hello")))))
+      
