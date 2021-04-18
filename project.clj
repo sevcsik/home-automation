29f603ef30993a93a805cd42de603107b0f34579
Sevcsik-Zajácz András <sevcsik-zajacz.andras@gbsolutions.io>
2021-04-18 14:07:55 +0200

    logging

--- project.clj
+++ project.clj
@@ -3,18 +3,30 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[clojurewerkz/machine_head "1.0.0"]
+  :dependencies [;; https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [ch.qos.logback/logback-core "1.2.3"]
+                 [clojurewerkz/machine_head "1.0.0"]
                  [org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "2.0.2"]
+                 [org.clojure/tools.logging "1.1.0"]
                  ;; https://mvnrepository.com/artifact/org.drools/drools-compiler
                  [org.drools/drools-compiler "7.50.0.t20210209"]
                  ;; https://mvnrepository.com/artifact/org.slf4j/slf4j-api
                  [org.slf4j/slf4j-api "1.7.30"]
-                 ;; https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
-                 [org.slf4j/slf4j-log4j12 "1.7.30"]
                  ;; https://mvnrepository.com/artifact/org.drools/drools-mvel
                  [org.drools/drools-mvel "7.50.0.t20210209"]]
-
+  :exclusions [;; Exclude transitive dependencies on all other logging
+               ;; implementations, including other SLF4J bridges.
+               commons-logging
+               log4j
+               org.apache.logging.log4j/log4j
+               org.slf4j/simple
+               org.slf4j/slf4j-jcl
+               org.slf4j/slf4j-nop
+               org.slf4j/slf4j-log4j12
+               org.slf4j/slf4j-log4j13]
+  :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]
   :main ^:skip-aot home-automation.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/home_automation/core.clj
+++ src/home_automation/core.clj
@@ -1,5 +1,6 @@
 (ns home-automation.core
   (:require [clojure.data.json :as json])
+  (:require [clojure.tools.logging :as log])
   (:require [clojure.string :as string])
   (:require [clojurewerkz.machine-head.client :as mh])
   (:gen-class))
@@ -18,10 +19,7 @@
     (.setFromMap fact-type instance {"source" source "action" action})
     instance))
 
-(defn is-action [ks obj] (let [result (= (type obj) (.getFactClass (get-fact-type ks "Action")))]
-                           (println "Result: " result)
-                           (result)))
-
+(defn is-action [ks obj] (= (type obj) (.getFactClass (get-fact-type ks "Action"))))
 
 (defn init-kie-container
   "Creates a new KIE container"
@@ -46,19 +44,24 @@
   (let [action ((json/read-str (String. payload-raw "UTF-8")) "action")
         source (string/replace topic "zigbee2mqtt/" "")
         event (create-event ks source action)]
+    (log/debug "Received event: " event)
     (.insert ks event)))
 
 (defn on-action [mqtt-conn obj] (println "OBJ: " obj))
 
+(def mqtt-url "tcp://localhost:1883")
+(def mqtt-topic "zigbee2mqtt")
 (defn -main
   [& args]
+  (log/infof "Connecting to mqtt on %s..." mqtt-url)
+  (log/info "Initialising Drools")
   (let [ks (init-knowledge-session (init-kie-container))
         mqtt-conn (mh/connect "tcp://localhost:1883")
         ks-cb (fn [evt] (let [obj (.getObject evt)] (when (is-action obj) (on-action mqtt-conn obj))))
         mqtt-cb (fn [topic _ payload] (on-message ks topic payload))]
-    (println "Initialising Drools & MQTT...")
-    (mh/subscribe mqtt-conn { "zigbee2mqtt/#" 0 } mqtt-cb)
+    (mh/subscribe mqtt-conn { (str mqtt-topic "/#") 0 } mqtt-cb)
     (ks-add-rule-runtime-event-listener ks ks-cb (constantly nil) (constantly nil))
+    (log/info "Listening on mqtt to topic" (str mqtt-topic "/#"))
     (Thread/sleep Long/MAX_VALUE))) ;TODO learn a better way to sleep forever
     
 
